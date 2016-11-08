acf34528950175e103d6979b837145eccce1a329
wmbotela <wmbotela@gmail.com>
2016-11-08 05:13:17 +0300

    this adds subtraction,multiplication,division functions to js

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,22 @@
+// addition function
 var add = function(number1, number2) {
         return number1 + number2;
       };
 
       var number1 = parseInt(prompt("Enter a number:"));
       var number2 = parseInt(prompt("Enter another number:"));
-      var result = add(number1, number2);
+// Subtraction function
+var subtract = function(number1,number2){
+              return number1-number2;
+};
+
+// Multiplication Function
+var multiply = function(number1,number2){
+              return number1-number2;
+};
+// Division function
+var divide = function(number1,number2){
+              return number1-number2;
+};
+      var result = divide(number1, number2);
       alert(result);
