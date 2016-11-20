ad781619e16d2106f8a4c0a8cc4ea835db8f07d1
wmbotela <wmbotela@gmail.com>
2016-11-20 15:06:10 +0300

    This adds the front end of thee calculator

--- js/scripts.js
+++ js/scripts.js
@@ -1,10 +1,8 @@
+// Business logic
 // addition function
 var add = function(number1, number2) {
-        return number1 + number2;
+        return number1+ number2;
       };
-
-      var number1 = parseInt(prompt("Enter a number:"));
-      var number2 = parseInt(prompt("Enter another number:"));
 // Subtraction function
 var subtract = function(number1,number2){
               return number1-number2;
@@ -12,11 +10,28 @@ var subtract = function(number1,number2){
 
 // Multiplication Function
 var multiply = function(number1,number2){
-              return number1-number2;
+              return number1*number2;
 };
 // Division function
 var divide = function(number1,number2){
-              return number1-number2;
+              return number1/number2;
 };
-      var result = divide(number1, number2);
-      alert(result);
+
+    // User interface
+
+      // var number1 = parseInt(prompt("Enter a number:"));
+      // var number2 = parseInt(prompt("Enter another number:"));
+      //
+      // var result = add(number1, number2);
+      // alert(result);
+
+      $(document).ready(function () {
+        $("form#add").submit(function(event){
+          event.preventDefult();
+          var number1 = parseInt($("#add1").val());
+          var number2 = parseInt($("#add2").val());
+          var result = add(number1,number2);
+          $("#output").text(result);
+
+        });
+      });
